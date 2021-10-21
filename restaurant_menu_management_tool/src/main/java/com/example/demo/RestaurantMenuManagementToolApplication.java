a8368aa0e20388d2acaa1742f94e83573bba1150
Warunee Dinunzio <dinunziow@gmail.com>
2021-10-21 16:13:51 -0400

    create project files

---
+++ restaurant_menu_management_tool/src/main/java/com/example/demo/RestaurantMenuManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantMenuManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantMenuManagementToolApplication.class, args);
+	}
+
+}
